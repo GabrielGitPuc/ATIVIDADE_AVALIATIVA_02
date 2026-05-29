e8852b869e80f80b2e0c0fb403463d19c766bb33
GabrielGitPuc <gabriel.githubpuc@gmail.com>
2026-05-29 10:16:02 -0300

    Fix random number generation in RO function

--- funcoes.py
+++ funcoes.py
@@ -2,13 +2,16 @@ import random
 import string
 import os
 
+
+def RO():
+    numeros_aleatorios = ''.join(random(map(str, random.sample(1,9), k=4)))
+    return f'{numeros_aleatorios}'
+
+
 def ID_pedido():
     letra_aleatoria = ''.join(random.sample(string.ascii_uppercase, k=1))
     numeros_aleatorios = ''.join(map(str, random.sample(range(1,9), k=4)))
     return f'{letra_aleatoria}{numeros_aleatorios}'
-
-
-
 
 
 def ID_entregador():
@@ -28,9 +31,6 @@ def limite_veiculo(veiculo):
             return 35
     else:  # Van
         return 3
-
-def verificar_limite(id_entregador, entregador):
-    return entregador[id_entregador]['pedidos_atuais'] < entregador[id_entregador]['limite']
 
 def limpar_menu():
     os.system('cls' if os.name == 'nt' else 'clear')
